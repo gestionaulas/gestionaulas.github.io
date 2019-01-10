6011becae67d40acd4d3dbfb58358766977e5781
LuisRamos284 <flamos284@gmail.com>
2019-01-10 17:32:03 -0400

    prueba reservar

--- js/general.js
+++ js/general.js
@@ -2,4 +2,22 @@ $('#InputStartDate').dateDropper();
 
 $('#InputEndDate').dateDropper();
 
-$('select').customSelect();
+function load() {
+    $("#content").load("newUser.html");
+  }
+
+function loadReserveClassRom() {
+      $("#content").load("classRoomReserve.html");  
+}
+
+  $(document).ready(function(){
+    $("#login").click(function(){
+       $("#content").load("login.html");
+    });
+
+    $(".nav-item").click(function(event){
+      $(".nav-item").removeClass("active"); 
+        $(this).addClass("active");           
+      }
+    );
+  });
