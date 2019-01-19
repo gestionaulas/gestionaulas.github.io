254be6f2f376bad0d70526e58261273d6ee28e6d
LuisRamos284 <flamos284@gmail.com>
2019-01-19 10:56:10 -0400

    ultima version estable

--- SGDA/__init__.py
+++ SGDA/__init__.py
@@ -1,12 +1,57 @@
 from flask import Flask
 from pymongo import MongoClient
 from flask_bcrypt import Bcrypt
+from flask_user import UserManager, UserMixin
+from flask_login import LoginManager
+from flask_mongoengine import MongoEngine, MongoEngineSessionInterface
+
+
+class ConfigClass(object):
+    """ Flask application config """
+
+    # Flask settings
+    SECRET_KEY = '776766d63eecce38ccea4f3ebb68c69b'
+
+    # Flask-MongoEngine settings
+    MONGODB_SETTINGS = {
+        'db': 'sgda',
+        'host': 'mongodb://localhost:27017/'
+    }
+
+    # Flask-User settings
+    USER_APP_NAME = "SGDA"      # Shown in and email templates and page footers
+    USER_ENABLE_EMAIL = False      # Disable email authentication
+    USER_ENABLE_USERNAME = True    # Enable username authentication
+    USER_REQUIRE_RETYPE_PASSWORD = False    # Simplify register form
+    USER_UNAUTHORIZED_ENDPOINT = 'home'
+    USER_UNAUTHENTICATED_ENDPOINT = 'login'
+    USER_AFTER_LOGIN_ENDPOINT = 'home'
+    USER_AFTER_LOGOUT_ENDPOINT = 'home'
+    
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '776766d63eecce38ccea4f3ebb68c69b'
+app.config.from_object(__name__+'.ConfigClass')
 client = MongoClient('localhost', 27017)
 db = client['sgda']
 bcrypt = Bcrypt(app)
+db = MongoEngine(app)
+
+class User(db.Document, UserMixin):
+        active = db.BooleanField(default=True)
+
+        # User authentication information
+        username = db.StringField(default='')
+        password = db.StringField()
+
+        # User information
+        first_name = db.StringField(default='')
+        last_name = db.StringField(default='')
+
+        # Relationships
+        roles = db.ListField(db.StringField(), default=[])
+
+user_manager = UserManager(app, db, User)
+app.session_interface = MongoEngineSessionInterface(db)
 
 
 from SGDA import routes
--- SGDA/routes.py
+++ SGDA/routes.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, url_for, flash, redirect, jsonify
 from SGDA import app, db, bcrypt
 from SGDA.forms import RegistrationForm, LoginForm
 from flask_bcrypt import Bcrypt
-from flask_user import login_required, UserManager, UserMixin
+from flask_user import login_required
+
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    classrooms = db.aulas.find()
     return render_template('home.html', classrooms=classrooms)
 
 
@@ -20,7 +21,7 @@ def register():
         password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         db.users.insert_one({ "email":email, "username":username, "password":password, "profile":"student"})  
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -29,6 +30,10 @@ def login():
     return render_template('login.html', form=form)
 
 @app.route("/members", methods=['GET', 'POST'])
+@login_required
 def members():
     form = LoginForm()
     return render_template('login.html', form=form)
+
+
+
