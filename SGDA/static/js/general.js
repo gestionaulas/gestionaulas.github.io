8824029d2657d063efefca1fbfe161393c30693e
LuisRamos284 <flamos284@gmail.com>
2019-01-20 23:17:17 -0400

    calendar

--- SGDA/static/js/general.js
+++ SGDA/static/js/general.js
@@ -16,12 +16,24 @@ function loadReserveClassRom() {
       $("#content").load("classRoomReserve.html");  
 }
 
+
   $(document).ready(function(){
     $("#login").click(function(){
       console.log("reserve");
        $("#content").load("login.html");
     });
 
+    $('#calendar').fullCalendar({
+      themeSystem: 'bootstrap4',
+      defaultView: 'agendaWeek',
+      locale: 'es',
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'agendaWeek'
+      },
+      events: 'https://fullcalendar.io/demo-events.json'
+    });
     $(".nav-item").click(function(event){
       $(".nav-item").removeClass("active"); 
         $(this).addClass("active");           
